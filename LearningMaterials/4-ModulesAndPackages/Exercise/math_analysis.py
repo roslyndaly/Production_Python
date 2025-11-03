1521831b4af0bb5ca3f8f99a5f972203efb295f7
Roslyn Daly <roslyndaly@kubrickgroup.com>
2025-11-03 16:34:31 +0000

    chnages to exercise 4

--- LearningMaterials/4-ModulesAndPackages/Exercise/math_analysis.py
+++ LearningMaterials/4-ModulesAndPackages/Exercise/math_analysis.py
@@ -1,6 +1,11 @@
-import src.math_operations as sm
+"""
+python file to perform mathematical operations
+"""
 
-print(sm.add(1,2))
-print(sm.subtract(1,2))
-print(sm.multiply(1,2))
-print(sm.divide(1,2))
+
+from src import math_operations as sm
+
+print(sm.add(1, 2))
+print(sm.subtract(1, 2))
+print(sm.multiply(1, 2))
+print(sm.divide(1, 2))
